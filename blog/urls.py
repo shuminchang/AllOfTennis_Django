f567c0e042e600c5d2c32c0819f6995c930be3d2
shuminchang <magic2313@gmail.com>
2023-09-27 16:26:55 +0800

    use test.html as homepage

--- blog/urls.py
+++ blog/urls.py
@@ -10,12 +10,12 @@ from .views import (
 from . import views
 
 urlpatterns = [
-    path("", PostListView.as_view(), name="blog-home"),
+    path("blog/", PostListView.as_view(), name="blog-home"),
     path("user/<str:username>", UserPostListView.as_view(), name="user-posts"),
     path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
     path("post/new/", PostCreateView.as_view(), name="post-create"),
     path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
     path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
     path("about/", views.about, name="blog-about"),
-    path("test/", views.test, name="blog-test")
+    path("", views.test, name="blog-test")
 ]
